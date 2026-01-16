24f04024efe97087682052e93728317f0e38cb0b
Ajay Kumar <ajaykumarmkaju@gmail.com>
2026-01-16 16:27:56 +0530

    Added Home page content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <div className="">Home Page</div>;
+  return (
+    <div className="grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2 h-screen place-items-center gap-4">
+      <div className="">CSS Cake Goes here</div>
+      <div className="flex justify-center items-center flex-col gap-4">
+        <h1 className="text-4xl">Birthday Wisher</h1>
+        <p className="text-2xl">Send birthday wishes to your loved ones</p>
+        <button>Create a Wish</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
