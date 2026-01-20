4976e980d4380887a9fc3c3270e4c34adbff5f1e
Ajay Kumar <ajaykumarmkaju@gmail.com>
2026-01-21 02:56:18 +0530

    Revert "setup shadcn/ui"
    This reverts commit 117d23f143cc28deb2d068a56c43ba6728a802a7.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import { resolve, dirname } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': resolve(dirname, './src'),
-    },
-  },
 });
