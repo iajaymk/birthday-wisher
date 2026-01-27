c5c5d8e290453c114de5b1eb031e7cd09d649ec1
Ajay Kumar <ajaykumarmkaju@gmail.com>
2026-01-27 18:19:55 +0530

    Add basic text and age display on wish page

--- src/pages/WishPage.jsx
+++ src/pages/WishPage.jsx
@@ -1,5 +1,28 @@
+import { useSearchParams } from 'react-router-dom';
+
 const WishPage = () => {
-  return <div>WishPage</div>;
+  const searchParams = useSearchParams();
+
+  const name = searchParams[0].get('name').toUpperCase() || 'Friend';
+  const age = searchParams[0].get('age') || 'unknown';
+
+  return (
+    <div className="min-h-screen gradient-hero relative overflow-hidden flex items-center justify-center p-6">
+      <div className="relative z-10 text-center max-w-lg mx-auto">
+        <div className="">
+          <h1 className="text-3xl md:text-4xl font-bold">
+            Happy Birthday, {name}!
+          </h1>
+
+          <div className="text-6xl md:text-7xl font-bold">{age}</div>
+
+          <div className="">
+            <p>Cake has to be displayed here</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WishPage;
